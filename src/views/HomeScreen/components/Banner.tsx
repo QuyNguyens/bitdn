49b7b2e69c322aaa82bf54c7483e83fd7cb71e2e
BitDN_QuyNguyens <quynguyen123alq@gmail.com>
2026-05-29 16:06:51 +0700

    update animation

--- src/views/HomeScreen/components/Banner.tsx
+++ src/views/HomeScreen/components/Banner.tsx
@@ -100,8 +100,8 @@ const Banner = () => {
           {slogan.map((word, index) => (
             <motion.span
               key={index}
-              initial={{ opacity: 0, y: 20, filter: 'blur(10px)' }}
-              animate={{ opacity: 1, y: 0, filter: 'blur(0px)' }}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
               transition={{
                 duration: 0.6,
                 delay: index * 0.15 + 0.5,
